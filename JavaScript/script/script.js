b4a17a9e2e289415c9f1ae50ccd2588f0cd32cd9
Mariana <maryhsiano@hotmail.com>
2023-02-10 18:27:33 -0300

    Comecando JavaScript

---
+++ JavaScript/script/script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Qual é o seu nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+
+console.log("Olá, " + nome + " " + sobrenome);
+
+document.write("<h1>Bem vindo " + nome + " " + sobrenome + ", ao curso de JavaScript!</h1>");
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />");
