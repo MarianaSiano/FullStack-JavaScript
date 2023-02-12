61430544f13c072151fbdfb67159b66d057f5a3b
Mariana <maryhsiano@hotmail.com>
2023-02-11 22:07:46 -0300

    Novas funcoes

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -1,3 +1,4 @@
+//Aula 1 de funções com JavaScript
 var area  = document.getElementById("area");
 
 function entrar()
@@ -26,4 +27,27 @@ function sair()
 {
     alert("Até mais!")
     area.innerHTML = "Você saiu do sistema!";
+}
+
+//Aula 2 de funções com JavaScript
+var nota = document.getElementById("nota");
+
+function mediaAluno(nota1, nota2)
+{
+    var nomeAluno = prompt("Qual o nome do aluno?");
+    var nota1, nota2;
+
+    nota1 = parseFloat(prompt("Digite a primeira nota do aluno "));
+    nota2 = parseFloat(prompt("Digite a segunda nota do aluno "));
+    var media = (nota1 + nota2) / 2;
+
+    if(media >= 7) {
+        document.write("Aprovado com a média " + media);
+    }
+
+    else {
+        document.write("Reprovado com a média " + media);
+    }
+
+    return media;
 }
