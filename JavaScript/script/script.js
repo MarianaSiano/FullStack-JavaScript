7c62546ee433008220a78b33a12ebfc349b083d0
Mariana <maryhsiano@hotmail.com>
2023-02-14 12:05:23 -0300

    Adicionando o gitignore

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -1,15 +1,2 @@
-//While
-/*
-var i = 0;
-while (i < 10) {
-    document.write("<br> O valor de i é: " + i);
-    i++;
-}
-*/
+//Switch
 
-//FOR = para
-var valor = 5;
-
-for(var i = 0; i < valor; i++){
-    document.write("<br> O valor de i é: " + i);
-}
