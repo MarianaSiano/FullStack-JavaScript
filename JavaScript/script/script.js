141e9fbde7fa900ed65d5cab19fe41be2697a0b3
Mariana <maryhsiano@hotmail.com>
2023-02-14 12:46:22 -0300

    Primeiro Switch

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -3,4 +3,27 @@
 function pedir()
 {
     var valor = prompt("Digite um valor entre 1 e 4");
+
+    /*
+    switch(Number(valor)){
+        case 1:
+            alert("Você escolheu suco");
+            break;
+
+        case 2:
+            alert("Você escolheu agua gelada");
+            break;
+
+        case 3:
+            alert("Você escolheu sorvete");
+            break;
+
+        case 4:
+            alert("Você escolheu chamar o garçom");
+            break;
+
+        default:
+            alert("Valor inválido! Tente novamente!");
+    }
+    */
 }
