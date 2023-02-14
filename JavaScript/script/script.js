5003df3b85cd11fc1fbb9b6b43586ccf40d7d03d
Mariana <maryhsiano@hotmail.com>
2023-02-14 14:06:37 -0300

    Projeto Calculadora IMC, mas não completamente funcional

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -8,3 +8,39 @@
 */
 
 //peso / (altura * altura)
+
+function calcularIMC(event)
+{
+    event.preventDefault();
+
+    var peso = document.getElementById("peso").value;
+    var altura = document.getElementById("altura").value;
+    var resultado = document.getElementById("resultado");
+    var imc = peso / (altura * altura);
+    var mensagem = "";
+
+    if(imc < 17) {
+        mensagem = "Muito abaixo do peso";
+        resultado.innerHTML = mensagem;
+    }
+
+    else if(imc >= 17 && imc <= 18.49) {
+        mensagem = "Abaixo do peso";
+        resultado.innerHTML = mensagem;
+    }
+
+    else if(imc >= 18.5 && imc <= 24.99) {
+        mensagem = "Peso normal";
+        resultado.innerHTML = mensagem;
+    }
+
+    else if(imc >= 25 && imc <= 29.99) {
+        mensagem = "Acima do peso";
+        resultado.innerHTML = mensagem;
+    }
+
+    else {
+        mensagem = "Obesidade";
+        resultado.innerHTML = mensagem;
+    }
+}
