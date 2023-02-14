3ec59c187d79ba40433170ff910bca8cc18e7a65
Mariana <maryhsiano@hotmail.com>
2023-02-14 11:57:11 -0300

    loop for

--- JavaScript/script/script.js
+++ JavaScript/script/script.js
@@ -1,7 +1,15 @@
 //While
+/*
+var i = 0;
+while (i < 10) {
+    document.write("<br> O valor de i é: " + i);
+    i++;
+}
+*/
 
-// var i = 0;
-// while (i < 10) {
-//     document.write("<br> O valor de i é: " + i);
-//     i++;
-// }
+//FOR = para
+var valor = 5;
+
+for(var i = 0; i < valor; i++){
+    document.write("<br> O valor de i é: " + i);
+}
