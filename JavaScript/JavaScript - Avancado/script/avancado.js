1632de2592c69efb2da7f0c8a0ed7b8ddb1a84e5
Mariana <maryhsiano@hotmail.com>
2023-02-15 16:19:57 -0300

    Gerando numeros, porem com numero quebrado

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,13 +1,30 @@
 //Rest Operator
 
-function convidados(...nomes)
-{
-    console.log("Seja bem vindos!! Todos Convidados");
-    console.log(...nomes);
+// function convidados(...nomes)
+// {
+//     console.log("Seja bem vindos!! Todos Convidados");
+//     console.log(...nomes);
 
-    document.write("Seja bem vindos!! Todos Convidados");
+//     document.write("Seja bem vindos!! Todos Convidados");
+//     document.write("<br>");
+//     document.write(...nomes + "<br>");
+// }
+
+// convidados("João", "Maria", "José", "Pedro", "Ana", "Paulo");
+
+function sorteador(...numeros)
+{
+    console.log("Sorteio de números");
+    console.log(...numeros);
+
+    document.write("Sorteio de números");
     document.write("<br>");
-    document.write(...nomes + "<br>");
+    document.write(...numeros + "<br>");
+
+    const numeroGerado = Math.random() * numeros.length;
+    console.log(numeroGerado);
+
+    document.write("Número sorteado: " + numeroGerado);
 }
 
-convidados("João", "Maria", "José", "Pedro", "Ana", "Paulo");
+sorteador(39, 20, 98, 46, 35, 42, 15, 53);
