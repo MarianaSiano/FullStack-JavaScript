fbb090b179111193b406e495dc779918f3c714ab
Mariana <maryhsiano@hotmail.com>
2023-02-15 14:20:35 -0300

    Template String

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,20 +1,10 @@
-//Objetos
+//Template string
+let nome = 'João';
+let sobrenome = 'Silva';
+let idade = 30;
 
-/*let pessoa = {
-    nome: 'João',
-    idade: 20,
-    altura: 1.80,
-    cargo: 'Programador Back-end',
-};
+//let mensagem = "Olá, " + nome + " " + sobrenome + ", você tem " + idade + " anos.
 
-let carro = {
-    nome: 'Fusca',
-    cor: 'Azul',
-    potencia: 1.0,
-};*/
+let mensagem = `Olá, ${nome} ${sobrenome}, você tem ${idade} anos.`;
 
-let usuarios = [
-    {nome: 'Maria', cargo: 'Programador Back-end', status: 'Ativo'},
-    {nome: 'João', cargo: 'Programador Front-end', status: 'Inativo'},
-    {nome: 'Pedro', cargo: 'Programador Full-stack', status: 'Ativo'},
-];
+document.write(mensagem);
