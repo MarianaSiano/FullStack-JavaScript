f2f215398fbecb1e6ca19edcbf429aa56d3e7ee7
Mariana <maryhsiano@hotmail.com>
2023-02-15 12:22:48 -0300

    setInterval e setTimout

---
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -0,0 +1,15 @@
+//setInterval
+function acao(){
+    document.write("Executando... <br>");
+}
+
+/*var time = setInterval(() => {
+    document.write("Executando... <br>");
+}, 1000);*/
+
+//setTimeout
+//setTimeout(acao, 3000);
+
+setTimeout(() => {
+    document.write("Executando... <br>");
+}, 3000);
