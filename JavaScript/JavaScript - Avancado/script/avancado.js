05461b6da3500ba80f5ff165c385347083915fd5
Mariana <maryhsiano@hotmail.com>
2023-02-15 14:32:40 -0300

    Descontruindo arrays

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -8,3 +8,16 @@ let pessoa = {
 // let nome = "Maria";
 
 // const {nome:nomePessoa, cargo, empresa, sobrenome} = pessoa;
+
+//==========================================================
+
+//Desconstruir um array
+let nomes = ["João", "Maria", "José"];
+
+// let { 0:joão} = nomes
+// document.write(joão);
+
+let [primeironome, segundonome] = nomes;
+document.write(primeironome);
+document.write(", ");
+document.write(segundonome);
