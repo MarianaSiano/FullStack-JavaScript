ee7b693d06599edb91a033a290e97b87fa640f05
Mariana <maryhsiano@hotmail.com>
2023-02-15 15:12:14 -0300

    Cadastro

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -5,16 +5,30 @@
 
 // console.log(numeros);
 
-let pessoa = {
-    nome: 'João',
-    idade: 20,
-    cargo: 'Desenvolvedor',
-};
+// let pessoa = {
+//     nome: 'João',
+//     idade: 20,
+//     cargo: 'Desenvolvedor',
+// };
 
-let novaPessoa = {
-    ...pessoa,
-    status: 'Ativo',
-    cisdade: 'São Paulo/SP',
-};
+// let novaPessoa = {
+//     ...pessoa,
+//     status: 'Ativo',
+//     cisdade: 'São Paulo/SP',
+// };
 
-console.log(novaPessoa);
+// console.log(novaPessoa);
+
+function novoUsuario(info)
+{
+    let dados = {
+        ...info,
+        status: 'Ativo',
+        inicio: "01/01/2020",
+        codigo: '123456'
+    };
+
+    console.log(dados);
+}
+
+novoUsuario({Nome: 'João', Idade: 20, Cargo: 'Desenvolvedor'})
