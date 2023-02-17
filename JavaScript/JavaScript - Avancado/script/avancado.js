5e7db7af13bf79d2f832538084bd8923cea5fed2
Mariana <maryhsiano@hotmail.com>
2023-02-17 17:27:06 -0300

    Comecando array

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,30 +1,7 @@
-//Rest Operator
+//map = percorrer um array
 
-// function convidados(...nomes)
-// {
-//     console.log("Seja bem vindos!! Todos Convidados");
-//     console.log(...nomes);
+let lista = ["Giovani, Quezia, Mateus"];
 
-//     document.write("Seja bem vindos!! Todos Convidados");
-//     document.write("<br>");
-//     document.write(...nomes + "<br>");
-// }
-
-// convidados("João", "Maria", "José", "Pedro", "Ana", "Paulo");
-
-function sorteador(...numeros)
-{
-    console.log("Sorteio de números");
-    console.log(...numeros);
-
-    document.write("Sorteio de números");
-    document.write("<br>");
-    document.write(...numeros + "<br>");
-
-    const numeroGerado = Math.floor(Math.random() * numeros.length);
-    console.log(numeros[numeroGerado]);
-
-    document.write("Número sorteado: " + numeros[numeroGerado]);
-}
-
-sorteador(39, 20, 98, 46, 35, 42, 15, 53);
+lista.map((item, index) => {
+    console.log(`Passando: ${item}`);
+})
