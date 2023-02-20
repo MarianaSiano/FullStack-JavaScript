2692348b12dde90d60294cd948771d2a6a56d355
Mariana <maryhsiano@hotmail.com>
2023-02-20 16:08:03 -0300

    Funcao filter

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,14 +1,25 @@
 //Find = Buscar alguma coisa
-let listagem = [5, 9, "Maria Alice", 14, "Ana Carolina"];
+// let listagem = [5, 9, "Maria Alice", 14, "Ana Carolina"];
 
-let busca = listagem.find((item) => {
-    if(item === "Maria Alice"){
+// let busca = listagem.find((item) => {
+//     if(item === "Maria Alice"){
+//         return item;
+//     }
+
+//     else{
+//         return "Não encontrado";
+//     }
+// })
+
+// document.write("O resultado da busca foi: " + busca);
+
+//Filter = Filtrar alguma coisa
+let palavras = ["Ana Vitória", "Maria", "Augusto", "Enzo Gabriel", "Luiz Miguel"];
+
+let resutado = palavras.filter((item) => {
+    if(item.length <= 5){
         return item;
-    }
-
-    else{
-        return "Não encontrado";
     }
 })
 
-document.write("O resultado da busca foi: " + busca);
+document.write("O resultado da busca foi: " + resutado);
