ecf1ac2712ab8ee868b977da2f2e8e747dcbe1be
Mariana <maryhsiano@hotmail.com>
2023-02-20 15:25:28 -0300

    Reduce

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,8 +1,18 @@
 //map = percorrer um array
 
-let lista = ["Giovani", "Quezia", "Mateus", "João", "Pedro"];
+// let lista = ["Giovani", "Quezia", "Mateus", "João", "Pedro"];
 
-lista.map((item, index) => {
-    console.log(`Passando: ${item} - Esta na posição: ${index} `);
-    document.write(`- Passando: ${item} - Esta na posição: ${index} <br>`);
-})
+// lista.map((item, index) => {
+//     console.log(`Passando: ${item} - Esta na posição: ${index} `);
+//     document.write(`- Passando: ${item} - Esta na posição: ${index} <br>`);
+// })
+
+//Reduce = reduzir um array
+let numeros = [25, 20, 21];
+
+let total = numeros.reduce((acumulador, numero, indice, original) => {
+    console.log(`Indice: ${indice} - Numero: ${numero} - Acumulador: ${acumulador}`);
+    return acumulador + numero;
+}, 0);
+
+document.write(`Total: ${total}`);
