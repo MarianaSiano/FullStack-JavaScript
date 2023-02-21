420bc7cea8731c9418a737627065d389bf111884
Mariana <maryhsiano@hotmail.com>
2023-02-21 12:48:44 -0300

    Funcao anonima com array

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -6,3 +6,13 @@ let subtrair = (valor1, valor2) => {
 }
 
 subtrair(35, 14);
+
+document.write("<br>");
+document.write("<br>");
+
+let numeros = [15, 21, 1, 32]
+
+numeros.map((item) => {
+    document.write(item);
+    document.write("<br>");
+})
