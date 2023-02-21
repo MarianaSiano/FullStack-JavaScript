8630c2a6d002f454ec1c807c55e0c8f59f94a587
Mariana <maryhsiano@hotmail.com>
2023-02-21 19:48:07 -0300

    comecando node

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const server = express()
 
+//Query params = ?nome=Node.js
+//Route params = /users/1
+//Request body = { nome: "Node.js", tipo: "Back-end"}
+
 //localhost:3000/curso
 server.get('/curso', (req, res) => {
+    const nome = req.query.nome;
     return res.json({ curso: 'Node.js' })
 })
 
