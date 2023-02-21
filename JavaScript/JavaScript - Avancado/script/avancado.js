1166f442b299eb179b3bff11e13dae536bea5ceb
Mariana <maryhsiano@hotmail.com>
2023-02-21 13:03:58 -0300

    Includes

--- JavaScript/JavaScript - Avancado/script/avancado.js
+++ JavaScript/JavaScript - Avancado/script/avancado.js
@@ -1,18 +1,16 @@
-//Funções Anônimas
+//includes, starWith e endsWith
 
-let subtrair = (valor1, valor2) => {
-    let total = valor1 - valor2;
-    document.write("Total da suntracao: " + total);
+//Includes
+let nomes = ["Lavínia", "Ana", "Alexia"]
+document.write(nomes.includes("Ana") + "<br>")
+document.write(nomes.includes("Alícia") + "<br>")
+
+document.write("<br>")
+
+if(nomes.includes("Lavínia")){
+    document.write("Lavínia está na lista de nomes" + "<br>")
 }
 
-subtrair(35, 14);
-
-document.write("<br>");
-document.write("<br>");
-
-let numeros = [15, 21, 1, 32]
-
-numeros.map((item) => {
-    document.write(item);
-    document.write("<br>");
-})
+else {
+    document.write("Lavínia não está na lista de nomes" + "<br>")
+}
