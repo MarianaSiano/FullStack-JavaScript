4411a560dc403325f37eb9eaf1f992b21bdd0d11
Mariana <maryhsiano@hotmail.com>
2023-02-24 12:49:04 -0300

    Uptade

--- Node.js/index.js
+++ Node.js/index.js
@@ -19,9 +19,18 @@ server.get('/cursos/:index', (req, res) => {
     return res.json(cursos[index]);
 })
 
+//Criando um novo curso
 server.post('/cursos', (req, res) => {
     const { nome } = req.body;
     cursos.push(nome);
+    return res.json(cursos);
+})
+
+//Atualizando um curso
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { nome } = req.body;
+    cursos[index] = nome;
     return res.json(cursos);
 })
 
