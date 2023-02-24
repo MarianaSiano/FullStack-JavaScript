b3fb38eb788b3cddfca9ae694728d7a2eb605c4b
Mariana <maryhsiano@hotmail.com>
2023-02-24 11:45:50 -0300

    Metodo get com insomnia

--- Node.js/index.js
+++ Node.js/index.js
@@ -5,10 +5,12 @@ const server = express()
 //Route params = /users/1
 //Request body = { nome: "Node.js", tipo: "Back-end"}
 
+const cursos = ['Node.js', 'React', 'React Native'];
+
 //localhost:3000/curso
-server.get('/curso/:id', (req, res) => {
-    const id = req.params.id;
-    return res.json({ curso: `Id do curso: ${id}` });
+server.get('/curso/:index', (req, res) => {
+    const { index } = req.params;
+    return res.json(cursos[index]);
 })
 
 server.listen(3000)
